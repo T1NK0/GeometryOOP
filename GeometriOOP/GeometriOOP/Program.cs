e99bcc594d123e85c0d1d6b3f917ae8f8730bfd7
Mads Tinko <50380211+T1NK0@users.noreply.github.com>
2020-08-25 21:08:07 +0200

    Add files via upload

---
+++ GeometriOOP/GeometriOOP/Program.cs
@@ -0,0 +1,47 @@
+﻿
+using System;
+
+namespace GeometriOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Opgave a
+            //Console.WriteLine("Indtast sidelængde for første kvadrat i cm: "); //Asks user to insert the first squares side length.
+            //double square1SideLength = double.Parse(Console.ReadLine()); //Sets the square1SideLength to the user input
+
+            //Square square1 = new Square(square1SideLength); //Creates our square1 object and sends the value the user inputted in the int square1SideLength with as the value.
+            //Console.WriteLine("Omkredsen af første kvadrat er: " + square1.PerimiterCalculator() + "cm"); //Writes the Perimeter of the square out to the console, by calling our PerimiterCalculator method in the console.writeline.
+            //Console.WriteLine("Arealet af første kvadrat er: " + square1.AreaCalculator() + "cm2");
+
+            //Console.WriteLine();
+
+            //Console.WriteLine("Indtast sidelængde for anden kvadrat i cm: ");
+            //double square2SideLength = double.Parse(Console.ReadLine());
+
+            //Square square2 = new Square(square2SideLength);
+            //Console.WriteLine("Omkredsen af anden kvadrat er: " + square2.PerimiterCalculator() + "cm");
+            //Console.WriteLine("Arealet af første kvadrat er: " + square2.AreaCalculator() + "cm2");
+
+            //Console.WriteLine();
+
+            //Console.WriteLine("Indtast sidelængde for tredje kvadrat i cm: ");
+            //double square3SideLength = double.Parse(Console.ReadLine());
+
+            //Square square3 = new Square(square3SideLength);
+            //Console.WriteLine("Omkredsen af tredje kvadrat er: " + square3.PerimiterCalculator() + "cm");
+            //Console.WriteLine("Arealet af første kvadrat er: " + square3.AreaCalculator() + "cm2");
+            #endregion
+
+            #region Opgave b
+            Square square4 = new Square();
+            square4.Side = 24.3;
+
+            Console.WriteLine("Side længde: " + square4.Side);
+            Console.WriteLine("Omkreds: " + square4.PerimiterCalculator());
+            Console.WriteLine("Areal: " + square4.AreaCalculator());
+            #endregion
+        }
+    }
+}
---
+++ GeometriOOP/GeometriOOP/Square.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeometriOOP
+{
+    public class Square
+    {
+        #region Opgave a
+        private double side; //Declares the variable side, which is the sides of our square, since they are all the same length we only need 1.
+
+        public double Side //Makes our side variable changeable.
+        {
+            get { return side; }
+            set { side = value; }
+        }
+
+        //public Square(double squareSide) //Creates our constructor
+        //{
+        //    side = squareSide; //Sets the "side" value to the user input we send over from the program.cs
+        //}
+
+        public double PerimiterCalculator() //Creates a method that allows us to calculate the Perimeter of the square called "PerimiterCalculator".
+        {
+            return side * 4; //Returns the result of our side * 4 which will give the Perimiter.
+        }
+
+        public double AreaCalculator() //Creates a method that allows us to calculate the area of the square called "AreaCalculator"
+        {
+            return side * 2; //Returns the value of the area by saying side * 2 since the side is the same all around.
+        }
+        #endregion
+
+        #region Opgave b
+        public Square() //Creates the empty constructor without any parameters
+        {
+
+        }
+        #endregion
+
+    }
+}
